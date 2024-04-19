20f837da02b8fe9bc383682ab5f54258f90681c7
grandeljay <github@grandels.email>
2024-04-19 09:24:37 +0200

    chore: move admin css into correct directory

--- src/admin/includes/extra/css/grandeljay_cao_product_measurements.php
+++ src/admin/includes/extra/css/grandeljay_cao_product_measurements.php
@@ -3,4 +3,4 @@
 use Grandeljay\AlwaysAvailable\Constants;
 
 ?>
-<link rel="stylesheet" href="css/grandeljay_cao_product_measurements.css?v=<?= Constants::MODULE_VERSION ?>">
+<link rel="stylesheet" href="includes/css/grandeljay_cao_product_measurements.css?v=<?= Constants::MODULE_VERSION ?>">
